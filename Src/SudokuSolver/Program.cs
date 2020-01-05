b46c5915a567fb592be5268686fc6a9942c732f4
Anders Juul <andersjuulsfirma@gmail.com>
2020-01-05 16:01:10 +0100

    Initial

---
+++ Src/SudokuSolver/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int size = 2;
+            int[,] array = new int[size, size];
+
+
+            for (var x = 0; x < size; x++)
+            {
+                for (var y = 0; y < size; y++)
+                {
+                    Console.Write(array[x, y]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
