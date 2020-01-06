ebf27577dccdce78290ee3848b414dc2141f9711
Anders Juul <andersjuulsfirma@gmail.com>
2020-01-06 10:22:04 +0100

    Initial

---
+++ Src/SudokuUI/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SudokuUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            var window = new MainWindow();
+            var viewModel = new MainWindowViewModel();
+            window.DataContext = viewModel;
+            window.Show();
+        }
+    }
+}
---
+++ Src/SudokuUI/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuUI
+{
+    public class MainWindowViewModel
+    {
+        public string Title => "Sudoku solver";
+    }
+}
