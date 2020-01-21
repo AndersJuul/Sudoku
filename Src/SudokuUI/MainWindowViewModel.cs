c53421fb2939d5d44852c5b014f13bb3517f168f
Anders Juul <andersjuulsfirma@gmail.com>
2020-01-21 15:08:16 +0100

    aaa

--- Src/SudokuUI/MainWindowViewModel.cs
+++ Src/SudokuUI/MainWindowViewModel.cs
@@ -7,5 +7,21 @@ namespace SudokuUI
     public class MainWindowViewModel
     {
         public string Title => "Sudoku solver";
+
+        public  int Rank => 3;
+
+        public string[,] Array { get; set; }
+
+        public MainWindowViewModel()
+        {
+            var array = new List<List<string>>();
+            for (int x = 0; x < Rank*Rank; x++)
+            {
+                for (int y = 0; y < Rank*Rank; y++)
+                {
+                    
+                }
+            }
+        }
     }
 }
